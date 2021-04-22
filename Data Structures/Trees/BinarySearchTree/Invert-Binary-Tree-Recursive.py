f225efda4fde8150e1a09d0e50f6dc81a5aaa1b8
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-23 00:12:48 +0200

    Create Invert-Binary-Tree-Recursive.py

---
+++ Data Structures/Trees/BinarySearchTree/Invert-Binary-Tree-Recursive.py
@@ -0,0 +1,13 @@
+# class Tree:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def solve(self, root):
+        if root is None:
+            return 
+        if root:
+            root.left,root.right = self.solve(root.right),self.solve(root.left)
+        return root
+        
