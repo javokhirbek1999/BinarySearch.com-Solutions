06016a5c7dc6d3f472cc791762dc9a920740ec33
Javokhirbek <54243963+javokhirbek1999@users.noreply.github.com>
2021-04-23 00:52:57 +0200

    Create Level-Order-Traversal-Tree-to-Linked-List.py

---
+++ Data Structures/Trees/Binary Tree/Level-Order-Traversal-Tree-to-Linked-List.py
@@ -0,0 +1,28 @@
+# class Tree:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class LLNode:
+#     def __init__(self, val, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def solve(self, root):
+        if root is None:
+            return None
+        dummy = LLNode(0)
+        tail = dummy
+
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            tail.next = LLNode(node.val)
+            tail = tail.next
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)    
+        return dummy.next        
+        
